b5ccf942301fd3b737ea26cd45021602936fcd43
Alex <alexxldub@gmail.com>
2020-08-09 22:46:37 +0300

    Updates

---
+++ precache-manifest.7c91cc518e111725757df88b64a89282.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aae459d4d28a84f4df10ecbf29b2807",
+    "url": "/my-blog-react/index.html"
+  },
+  {
+    "revision": "8e6ef63020e327f95c2a",
+    "url": "/my-blog-react/static/css/main.3d38f4e4.chunk.css"
+  },
+  {
+    "revision": "6a0b686f5781f1e81b5d",
+    "url": "/my-blog-react/static/js/0.9ad50aae.chunk.js"
+  },
+  {
+    "revision": "a6ac2a5315b3b0988bcf",
+    "url": "/my-blog-react/static/js/1.de80504c.chunk.js"
+  },
+  {
+    "revision": "cae7b1363272086cc255",
+    "url": "/my-blog-react/static/js/10.f226043e.chunk.js"
+  },
+  {
+    "revision": "fe0df8d6ca9aba4a67d6",
+    "url": "/my-blog-react/static/js/11.5c7c4b34.chunk.js"
+  },
+  {
+    "revision": "94c45e4b57a38a1c4eee",
+    "url": "/my-blog-react/static/js/12.ddb8a5af.chunk.js"
+  },
+  {
+    "revision": "fce62b1752b5ed33e970",
+    "url": "/my-blog-react/static/js/4.0beb3925.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/my-blog-react/static/js/4.0beb3925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34610f304536ad61cdcd",
+    "url": "/my-blog-react/static/js/5.a72346da.chunk.js"
+  },
+  {
+    "revision": "ebb5df190e3f325b1363b6d085edcca9",
+    "url": "/my-blog-react/static/js/5.a72346da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874b6ff77f4722bd80e1",
+    "url": "/my-blog-react/static/js/6.436a7b9d.chunk.js"
+  },
+  {
+    "revision": "bcb778a8303bcae1820df0fc7c763feb",
+    "url": "/my-blog-react/static/js/6.436a7b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5d0292990ff254533c",
+    "url": "/my-blog-react/static/js/7.9b895afb.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/my-blog-react/static/js/7.9b895afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6109fccbb80bbf75950",
+    "url": "/my-blog-react/static/js/8.4d6b8e92.chunk.js"
+  },
+  {
+    "revision": "d78360f408015ad7578b",
+    "url": "/my-blog-react/static/js/9.aca5343d.chunk.js"
+  },
+  {
+    "revision": "8e6ef63020e327f95c2a",
+    "url": "/my-blog-react/static/js/main.35f98387.chunk.js"
+  },
+  {
+    "revision": "1406592ea038282cf38b",
+    "url": "/my-blog-react/static/js/runtime-main.83bf75da.js"
+  },
+  {
+    "revision": "6339eb6f5e757b51e27664784c37878a",
+    "url": "/my-blog-react/static/media/3d.6339eb6f.png"
+  },
+  {
+    "revision": "5a0a0db53d8a3b53b226d05405abfc05",
+    "url": "/my-blog-react/static/media/404.5a0a0db5.png"
+  },
+  {
+    "revision": "a1a0dcd48a00fef9a5decb5b23e0c53b",
+    "url": "/my-blog-react/static/media/Cool.a1a0dcd4.png"
+  },
+  {
+    "revision": "541a7baf0665f92e4f33af7430e0a192",
+    "url": "/my-blog-react/static/media/Dima.541a7baf.jpg"
+  },
+  {
+    "revision": "c7a60f761d094cd620baaed7b8c0dd30",
+    "url": "/my-blog-react/static/media/EarthTexture2.c7a60f76.jpg"
+  },
+  {
+    "revision": "f7cfa6084cecaf59eb32b0f1f2f250c7",
+    "url": "/my-blog-react/static/media/Landing.f7cfa608.png"
+  },
+  {
+    "revision": "bb869dd2ee843648c1618731ddf12151",
+    "url": "/my-blog-react/static/media/Oleksander.bb869dd2.jpg"
+  },
+  {
+    "revision": "1243c2c8d4ab3f8a192e3f5c9e5f5776",
+    "url": "/my-blog-react/static/media/Roma.1243c2c8.jpg"
+  },
+  {
+    "revision": "8567583d2e33fd8d362aae74bc3a2e58",
+    "url": "/my-blog-react/static/media/STARWARS.8567583d.woff"
+  },
+  {
+    "revision": "10550ee86b47ea33b21c4df8c6a053e8",
+    "url": "/my-blog-react/static/media/css.10550ee8.png"
+  },
+  {
+    "revision": "a675cb93b75d5f1656c920dceecdcb38",
+    "url": "/my-blog-react/static/media/dart.a675cb93.png"
+  },
+  {
+    "revision": "c4f2ef5fb9ed8111151fd539c1f5910f",
+    "url": "/my-blog-react/static/media/dart.c4f2ef5f.png"
+  },
+  {
+    "revision": "363b3f3aa8383e40cc2d32ccde414f97",
+    "url": "/my-blog-react/static/media/defender.363b3f3a.png"
+  },
+  {
+    "revision": "1ca55f5f5baa576c4b893b80f4151513",
+    "url": "/my-blog-react/static/media/earth.1ca55f5f.png"
+  },
+  {
+    "revision": "2d78f49311cf0c079ea8aa1cae1a9de4",
+    "url": "/my-blog-react/static/media/github.2d78f493.png"
+  },
+  {
+    "revision": "65a041bcac40cc15e73cabab86f81e18",
+    "url": "/my-blog-react/static/media/github.65a041bc.png"
+  },
+  {
+    "revision": "ce199b2e785a2abf0b028f986d1fcae3",
+    "url": "/my-blog-react/static/media/gmail.ce199b2e.png"
+  },
+  {
+    "revision": "a00831a712bb4f59eba35cb2c4c7a737",
+    "url": "/my-blog-react/static/media/js.a00831a7.png"
+  },
+  {
+    "revision": "cd53cb57fea82009bdf4888b3f6a394c",
+    "url": "/my-blog-react/static/media/jupiterTexture.cd53cb57.jpg"
+  },
+  {
+    "revision": "a9d747921f6d2f11be42a7ddefa8c698",
+    "url": "/my-blog-react/static/media/logo.a9d74792.png"
+  },
+  {
+    "revision": "c00c3bdc45bde7327fca2dede470692a",
+    "url": "/my-blog-react/static/media/marsTexture.c00c3bdc.png"
+  },
+  {
+    "revision": "7b3ca609ea5dd5e8ea19489dbdf5302a",
+    "url": "/my-blog-react/static/media/mercuryTexture.7b3ca609.jpg"
+  },
+  {
+    "revision": "2d5f398aae53f8cf7856690b79bb139a",
+    "url": "/my-blog-react/static/media/music.2d5f398a.gif"
+  },
+  {
+    "revision": "c421995c13858d3d13149ba59e369fcc",
+    "url": "/my-blog-react/static/media/neptuneTexture.c421995c.jpg"
+  },
+  {
+    "revision": "5d1f309c44dede52ffd977ea6d31288f",
+    "url": "/my-blog-react/static/media/plutoTexture.5d1f309c.jpg"
+  },
+  {
+    "revision": "0cfaf7bb4ca6e836a852278d0e353f80",
+    "url": "/my-blog-react/static/media/preloader.0cfaf7bb.gif"
+  },
+  {
+    "revision": "6281dd1ec209fae40644e3f13c691811",
+    "url": "/my-blog-react/static/media/renKaulo.6281dd1e.png"
+  },
+  {
+    "revision": "bd9e7c92e3b55dcef3a5bf8164dddc64",
+    "url": "/my-blog-react/static/media/sectionDevider.bd9e7c92.png"
+  },
+  {
+    "revision": "f804e422a8241dc17edd964b63dfd22f",
+    "url": "/my-blog-react/static/media/slow.f804e422.gif"
+  },
+  {
+    "revision": "772e8a9588377a4928ff22d95d97e617",
+    "url": "/my-blog-react/static/media/socialNetwork.772e8a95.jpg"
+  },
+  {
+    "revision": "406c98539604ca30ce958395e9f48e51",
+    "url": "/my-blog-react/static/media/solarSys.406c9853.png"
+  },
+  {
+    "revision": "3a7949e7602a02361c048a0c689c3622",
+    "url": "/my-blog-react/static/media/starSword.3a7949e7.png"
+  },
+  {
+    "revision": "b56199e104c3daccad83373909495a59",
+    "url": "/my-blog-react/static/media/sunTexture.b56199e1.png"
+  },
+  {
+    "revision": "783a4860ecd7d9bf097aaa4a94c36d46",
+    "url": "/my-blog-react/static/media/telegram.783a4860.png"
+  },
+  {
+    "revision": "9b5e76eb22c60fcdf5db88af4da119c3",
+    "url": "/my-blog-react/static/media/telegramBot.9b5e76eb.png"
+  },
+  {
+    "revision": "883761fd003db251a25fde948d0a4198",
+    "url": "/my-blog-react/static/media/uranTexture.883761fd.png"
+  },
+  {
+    "revision": "6bf360922318937f53cb161081a953e0",
+    "url": "/my-blog-react/static/media/venusTexture.6bf36092.jpg"
+  }
+]);
